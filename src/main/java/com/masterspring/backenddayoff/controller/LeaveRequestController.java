519b007423fb82b26d631a252fd9e18a6608e83f
ntandat <20130221@st.hcmuaf.edu.vn>
2024-04-12 10:37:00 +0700

    Add create leave request function

--- src/main/java/com/masterspring/backenddayoff/controller/LeaveRequestController.java
+++ src/main/java/com/masterspring/backenddayoff/controller/LeaveRequestController.java
@@ -1,8 +1,12 @@
 package com.masterspring.backenddayoff.controller;
 
 import com.masterspring.backenddayoff.dto.LeaveRequestStatusDto;
+import com.masterspring.backenddayoff.dto.request.LeaveRequestPost;
+import com.masterspring.backenddayoff.dto.response.LeaveRequestPostResponse;
 import com.masterspring.backenddayoff.service.LeaveRequestService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +18,11 @@ public class LeaveRequestController {
     @Autowired
     public LeaveRequestController(LeaveRequestService leaveRequestService) {
         this.leaveRequestService = leaveRequestService;
+    }
+
+    @PostMapping
+    public ResponseEntity<LeaveRequestPostResponse> postLeaveRequest(@Valid @RequestBody LeaveRequestPost leaveRequestPost) {
+        return new ResponseEntity<>(leaveRequestService.postLeaveRequest(leaveRequestPost), HttpStatus.CREATED);
     }
 
     @PutMapping("/update_status/{id}")
--- src/main/java/com/masterspring/backenddayoff/service/LeaveRequestService.java
+++ src/main/java/com/masterspring/backenddayoff/service/LeaveRequestService.java
@@ -1,7 +1,11 @@
 package com.masterspring.backenddayoff.service;
 
 import com.masterspring.backenddayoff.dto.LeaveRequestStatusDto;
+import com.masterspring.backenddayoff.dto.request.LeaveRequestPost;
+import com.masterspring.backenddayoff.dto.response.LeaveRequestPostResponse;
 
 public interface LeaveRequestService {
+    LeaveRequestPostResponse postLeaveRequest(LeaveRequestPost leaveRequestPost);
+
     LeaveRequestStatusDto confirmLeaveRequest(Long id, LeaveRequestStatusDto leaveRequestDto);
 }
