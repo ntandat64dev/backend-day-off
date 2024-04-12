902830b7acaeb1340192aca9f913a8d782372dcc
ntandat <20130221@st.hcmuaf.edu.vn>
2024-04-12 09:17:11 +0700

    Init project

---
+++ src/main/java/com/masterspring/backenddayoff/BackendDayOffApplication.java
@@ -0,0 +1,13 @@
+package com.masterspring.backenddayoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDayOffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDayOffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/masterspring/backenddayoff/BackendDayOffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masterspring.backenddayoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDayOffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
