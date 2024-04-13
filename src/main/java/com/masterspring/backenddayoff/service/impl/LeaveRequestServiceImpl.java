87250a99c380b268474cd684ac9d120d1203c0c0
ntandat <20130221@st.hcmuaf.edu.vn>
2024-04-13 09:44:11 +0700

    Fix bug

--- src/main/java/com/masterspring/backenddayoff/service/impl/LeaveRequestServiceImpl.java
+++ src/main/java/com/masterspring/backenddayoff/service/impl/LeaveRequestServiceImpl.java
@@ -49,7 +49,7 @@ public class LeaveRequestServiceImpl implements LeaveRequestService {
         int remainDays = leaveRemain.getRemainDays();
         var starDateTime = leaveRequestPost.getStartDate().atTime(0, 0, 0);
         var endDateTime = leaveRequestPost.getEndDate().atTime(23, 59, 59);
-        int days = (int) Duration.between(starDateTime, endDateTime).toDays();
+        int days = (int) Duration.between(starDateTime, endDateTime).toDays() + 1;
         if (days > remainDays) {
             throw new AppException(400, "Request days exceeds remain days. (Remain days: %s, request days: %s)".formatted(remainDays, days));
         }
